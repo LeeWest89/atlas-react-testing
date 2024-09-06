14e63ac4e435dcd8b8b0f52d047e19958bd1977a
LeeWest89 <6683@holbertonstudents.com>
2024-09-06 12:06:26 -0500

    CoverArt is now displaying placeHolder as a square at what I believe to be the right size

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Footer from "./components/Footer";
-import MusicPlayer from "./MusicPlayer";
+import CoverArt from "./components/CoverArt";
 
 function App() {
   return (
     <div className="h-full flex flex-col justify-between p-8 min-h-screen">
-      <MusicPlayer />
+      <CoverArt />
       <Footer />
     </div>
   );
---
+++ src/components/CoverArt.jsx
@@ -0,0 +1,9 @@
+import placeHolder from '../assets/placeholder.svg';
+
+export default function CoverArt () {
+  return (
+    <div className='flex w-[27.25rem] h-[26.375rem]'>
+      <img src={placeHolder} alt="placeholder" />
+    </div>
+  );
+}
