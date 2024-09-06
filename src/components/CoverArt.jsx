b70bb680eecbbabceb0ae553a8a1abe6c2477752
LeeWest89 <6683@holbertonstudents.com>
2024-09-06 12:28:14 -0500

    having issues switching from docker

--- src/components/CoverArt.jsx
+++ src/components/CoverArt.jsx
@@ -2,7 +2,7 @@ import placeHolder from '../assets/placeholder.svg';
 
 export default function CoverArt () {
   return (
-    <div className='flex w-[27.25rem] h-[26.375rem]'>
+    <div className='flex h-[26.375rem] w-[27.25rem]'>
       <img src={placeHolder} alt="placeholder" />
     </div>
   );
---
+++ src/components/SongTitle.jsx
@@ -0,0 +1,5 @@
+export default function SongTitle () {
+  return (
+    <h1 className="font-bold ">Tidal Drift</h1>
+  );
+}
