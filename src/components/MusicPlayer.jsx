320236c1daf5abc409ddfab38f624a98e00c362e
LeeWest89 <lee.west@atlasschool.com>
2024-09-06 22:36:49 -0500

    finally got everything right now just to add color

--- src/components/MusicPlayer.jsx
+++ src/components/MusicPlayer.jsx
@@ -3,12 +3,10 @@ import Playlist from "./Playlist";
 
 export default function MusicPlayer() {
   return (
-    <div className="flex flex-col items-center justify-between min-h-[40rem] shadow-lg rounded-lg max-w-[42.9rem] md:flex-row md:max-w-[56rem]">
+    <div className="flex flex-col max-w-[42.9rem] min-h-[40rem] shadow-lg rounded-lg md:max-w-[56rem] md:w-full md:flex-row">
       <CurrentlyPlaying />
-
       {/* Vertical gray border */}
-      <div className="border-b-2 border-gray-200 w-full md:w-0 md:h-[40rem] md:border-l-2"></div>
-
+      <div className="border-b-2 border-gray-200 w-full md:h-[40rem] md:w-0 md:border-l-2"></div>
       <Playlist />
     </div>
   );
